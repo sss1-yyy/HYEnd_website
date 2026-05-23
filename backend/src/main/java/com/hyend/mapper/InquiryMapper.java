162ede46acf763954a591f0f7e9c910251b32f3a
sss1-yyy <ksui9210@gmail.com>
2026-05-24 03:36:02 +0900

    fix:inquiryMapper

--- backend/src/main/java/com/hyend/mapper/InquiryMapper.java
+++ backend/src/main/java/com/hyend/mapper/InquiryMapper.java
@@ -7,14 +7,18 @@ import com.hyend.dto.inquiry.ReplyResponse;
 import com.hyend.entity.Inquiry;
 import com.hyend.entity.InquiryReply;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 // TODO [H-3] 문의 MapStruct Mapper 구현
-@Mapper(componentModel="spring")
+@Mapper(componentModel = "spring")
 public interface InquiryMapper {
+
     Inquiry toEntity(InquiryRequest request);
+
+    @Mapping(target = "author", source = "author.name")
     InquiryResponse toResponse(Inquiry inquiry);
 
     InquiryReply toEntity(ReplyRequest request);
-    ReplyResponse toResponse(InquiryReply inquiry);
 
+    ReplyResponse toResponse(InquiryReply inquiry);
 }
