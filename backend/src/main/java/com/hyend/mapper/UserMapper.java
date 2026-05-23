005a6ec81702eefe90b5c7289a9387aeb8e9a285
sss1-yyy <ksui9210@gmail.com>
2026-05-24 03:43:08 +0900

    fix:usermapper

--- backend/src/main/java/com/hyend/mapper/UserMapper.java
+++ backend/src/main/java/com/hyend/mapper/UserMapper.java
@@ -5,8 +5,7 @@ import com.hyend.entity.User;
 import org.mapstruct.Mapper;
 
 // TODO [H-3] 사용자 MapStruct Mapper 구현
-@Mapper(componentModel="spring")
+@Mapper(componentModel = "spring")
 public interface UserMapper {
-    User toEntity(RegisterRequest request);
 
 }
